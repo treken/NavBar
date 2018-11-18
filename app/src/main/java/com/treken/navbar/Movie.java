treken/SomeApp

Fetch movies from TMDb by creating data fetching adapter and implementing api method.

app/src/main/java/com/treken/someapp/Movie.java
@@ -1,0 +1,81 @@
+package com.treken.someapp;
+
+import com.google.gson.annotations.Expose;
+import com.google.gson.annotations.SerializedName;
+
+import java.util.List;
+
+public class Movie {
+
+    @SerializedName("id")
+    @Expose
+    private int id;
+
+    @SerializedName("title")
+    @Expose
+    private String title;
+
+    @SerializedName("poster_path")
+    @Expose
+    private String posterPath;
+
+    @SerializedName("release_date")
+    @Expose
+    private String releaseDate;
+
+    @SerializedName("vote_average")
+    @Expose
+    private float rating;
+
+    @SerializedName("genre_ids")
+    @Expose
+    private List<Integer> genreIds;
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getTitle() {
+        return title;
+    }
+
+    public void setTitle(String title) {
+        this.title = title;
+    }
+
+    public String getPosterPath() {
+        return posterPath;
+    }
+
+    public void setPosterPath(String posterPath) {
+        this.posterPath = posterPath;
+    }
+
+    public String getReleaseDate() {
+        return releaseDate;
+    }
+
+    public void setReleaseDate(String releaseDate) {
+        this.releaseDate = releaseDate;
+    }
+
+    public float getRating() {
+        return rating;
+    }
+
+    public void setRating(float rating) {
+        this.rating = rating;
+    }
+
+    public List<Integer> getGenreIds() {
+        return genreIds;
+    }
+
+    public void setGenreIds(List<Integer> genreIds) {
+        this.genreIds = genreIds;
+    }
+}

app/src/main/java/com/treken/someapp/MoviesResponse.java
@@ -1,0 +1,57 @@
+package com.treken.someapp;
+
+import com.google.gson.annotations.Expose;
+import com.google.gson.annotations.SerializedName;
+
+import java.util.List;
+
+public class MoviesResponse {
+
+    @SerializedName("page")
+    @Expose
+    private int page;
+
+    @SerializedName("total_results")
+    @Expose
+    private int totalResults;
+
+    @SerializedName("results")
+    @Expose
+    private List<Movie> movies;
+
+    @SerializedName("total_pages")
+    @Expose
+    private int totalPages;
+
+    public int getPage() {
+        return page;
+    }
+
+    public void setPage(int page) {
+        this.page = page;
+    }
+
+    public int getTotalResults() {
+        return totalResults;
+    }
+
+    public void setTotalResults(int totalResults) {
+        this.totalResults = totalResults;
+    }
+
+    public List<Movie> getMovies() {
+        return movies;
+    }
+
+    public void setMovies(List<Movie> movies) {
+        this.movies = movies;
+    }
+
+    public int getTotalPages() {
+        return totalPages;
+    }
+
+    public void setTotalPages(int totalPages) {
+        this.totalPages = totalPages;
+    }
+}

app/src/main/java/com/treken/someapp/OnGetMoviesCallback.java
@@ -1,0 +1,10 @@
+package com.treken.someapp;
+
+import java.util.List;
+
+public interface OnGetMoviesCallback {
+
+    void onSuccess(List<Movie> movies);
+
+    void onError();
+}

treken/NavBar

app/src/main/java/com/treken/navbar/Movie.java
@@ -1,0 +1,81 @@
+package com.treken.navbar;
+
+import com.google.gson.annotations.Expose;
+import com.google.gson.annotations.SerializedName;
+
+import java.util.List;
+
+public class Movie {
+
+    @SerializedName("id")
+    @Expose
+    private int id;
+
+    @SerializedName("title")
+    @Expose
+    private String title;
+
+    @SerializedName("poster_path")
+    @Expose
+    private String posterPath;
+
+    @SerializedName("release_date")
+    @Expose
+    private String releaseDate;
+
+    @SerializedName("vote_average")
+    @Expose
+    private float rating;
+
+    @SerializedName("genre_ids")
+    @Expose
+    private List<Integer> genreIds;
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getTitle() {
+        return title;
+    }
+
+    public void setTitle(String title) {
+        this.title = title;
+    }
+
+    public String getPosterPath() {
+        return posterPath;
+    }
+
+    public void setPosterPath(String posterPath) {
+        this.posterPath = posterPath;
+    }
+
+    public String getReleaseDate() {
+        return releaseDate;
+    }
+
+    public void setReleaseDate(String releaseDate) {
+        this.releaseDate = releaseDate;
+    }
+
+    public float getRating() {
+        return rating;
+    }
+
+    public void setRating(float rating) {
+        this.rating = rating;
+    }
+
+    public List<Integer> getGenreIds() {
+        return genreIds;
+    }
+
+    public void setGenreIds(List<Integer> genreIds) {
+        this.genreIds = genreIds;
+    }
+}

app/src/main/java/com/treken/navbar/MoviesResponse.java
@@ -1,0 +1,57 @@
+package com.treken.navbar;
+
+import com.google.gson.annotations.Expose;
+import com.google.gson.annotations.SerializedName;
+
+import java.util.List;
+
+public class MoviesResponse {
+
+    @SerializedName("page")
+    @Expose
+    private int page;
+
+    @SerializedName("total_results")
+    @Expose
+    private int totalResults;
+
+    @SerializedName("results")
+    @Expose
+    private List<Movie> movies;
+
+    @SerializedName("total_pages")
+    @Expose
+    private int totalPages;
+
+    public int getPage() {
+        return page;
+    }
+
+    public void setPage(int page) {
+        this.page = page;
+    }
+
+    public int getTotalResults() {
+        return totalResults;
+    }
+
+    public void setTotalResults(int totalResults) {
+        this.totalResults = totalResults;
+    }
+
+    public List<Movie> getMovies() {
+        return movies;
+    }
+
+    public void setMovies(List<Movie> movies) {
+        this.movies = movies;
+    }
+
+    public int getTotalPages() {
+        return totalPages;
+    }
+
+    public void setTotalPages(int totalPages) {
+        this.totalPages = totalPages;
+    }
+}

app/src/main/java/com/treken/navbar/OnGetMoviesCallback.java
@@ -1,0 +1,10 @@
+package com.treken.navbar;
+
+import java.util.List;
+
+public interface OnGetMoviesCallback {
+
+    void onSuccess(List<Movie> movies);
+
+    void onError();
+}