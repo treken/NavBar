3493f7c5abf56d077a6f690149efd5d3b4c2004e
treken <kevinchupe@icloud.com>
2018-12-08 22:13:01 -0600

    Remove floating action button and extract movie container.

--- app/src/main/java/com/treken/cyberflix/MainActivity.java
+++ app/src/main/java/com/treken/cyberflix/MainActivity.java
@@ -1,9 +1,6 @@
 package com.treken.cyberflix;
 
 import android.os.Bundle;
-import android.support.design.widget.FloatingActionButton;
-import android.support.design.widget.Snackbar;
-import android.view.View;
 import android.support.design.widget.NavigationView;
 import android.support.v4.view.GravityCompat;
 import android.support.v4.widget.DrawerLayout;
@@ -25,14 +22,10 @@ public class MainActivity extends AppCompatActivity
         Toolbar toolbar = (Toolbar) findViewById( R.id.toolbar );
         setSupportActionBar( toolbar );
 
-        FloatingActionButton fab = (FloatingActionButton) findViewById( R.id.fab );
-        fab.setOnClickListener( new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                Snackbar.make( view, "Replace with your own action", Snackbar.LENGTH_LONG )
-                        .setAction( "Action", null ).show();
-            }
-        } );
+        if (savedInstanceState == null) {
+            MoviesFragment moviesFragment = MoviesFragment.newInstance();
+            getSupportFragmentManager().beginTransaction().add( R.id.container, moviesFragment ).commit();
+        }
 
         DrawerLayout drawer = (DrawerLayout) findViewById( R.id.drawer_layout );
         ActionBarDrawerToggle toggle = new ActionBarDrawerToggle(
@@ -42,11 +35,6 @@ public class MainActivity extends AppCompatActivity
 
         NavigationView navigationView = (NavigationView) findViewById( R.id.nav_view );
         navigationView.setNavigationItemSelectedListener( this );
-
-        if (savedInstanceState == null) {
-            MoviesFragment moviesFragment = MoviesFragment.newInstance();
-            getSupportFragmentManager().beginTransaction().add( R.id.container, moviesFragment ).commit();
-        }
     }
 
     @Override
