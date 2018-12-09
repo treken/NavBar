f3fceaac786ff3a6150cd0a804b13cb84fdace31
treken <kevinchupe@icloud.com>
2018-12-08 18:43:22 -0600

    Revert "Load movie posters using Glide."
    This reverts commit 879242392d65644261afc6b1e7cac5c2b187023d.

--- app/src/main/java/com/treken/cyberflix/MoviesAdapter.java
+++ app/src/main/java/com/treken/cyberflix/MoviesAdapter.java
@@ -4,16 +4,11 @@ import android.support.v7.widget.RecyclerView;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
-import android.widget.ImageView;
 import android.widget.TextView;
-
-import com.bumptech.glide.Glide;
-import com.bumptech.glide.request.RequestOptions;
 
 import java.util.List;
 
 public class MoviesAdapter extends RecyclerView.Adapter<MoviesAdapter.MovieViewHolder> {
-    private String IMAGE_BASE_URL = "http://image.tmdb.org/t/p/w500";
 
     private List<Movie> movies;
 
@@ -41,27 +36,21 @@ public class MoviesAdapter extends RecyclerView.Adapter<MoviesAdapter.MovieViewH
         TextView releaseDate;
         TextView title;
         TextView rating;
-        TextView genres; // TODO: Add genres
-        ImageView poster;
+        TextView genres;
 
         public MovieViewHolder(View itemView) {
             super(itemView);
-            releaseDate = itemView.findViewById( com.treken.navbar.R.id.item_movie_release_date);
-            title = itemView.findViewById( com.treken.navbar.R.id.item_movie_title);
-            rating = itemView.findViewById( com.treken.navbar.R.id.item_movie_rating);
-            genres = itemView.findViewById( com.treken.navbar.R.id.item_movie_genre); // Add genres later
-            poster = itemView.findViewById( com.treken.navbar.R.id.item_movie_poster );
+            releaseDate = itemView.findViewById(R.id.item_movie_release_date);
+            title = itemView.findViewById(R.id.item_movie_title);
+            rating = itemView.findViewById(R.id.item_movie_rating);
+            genres = itemView.findViewById(R.id.item_movie_genre);
         }
 
         public void bind(Movie movie) {
             releaseDate.setText(movie.getReleaseDate().split("-")[0]);
             title.setText(movie.getTitle());
             rating.setText(String.valueOf(movie.getRating()));
-            genres.setText(""); // Add genres later
-            Glide.with(itemView)
-                    .load( IMAGE_BASE_URL + movie.getPosterPath() )
-                    .apply( RequestOptions.placeholderOf( com.treken.navbar.R.color.colorPrimary ) )
-                    .into( poster );
+            genres.setText("");
         }
     }
 }
