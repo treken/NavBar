89801b639cab8b3461db0fb3ac65cd4e8c51520c
treken <kevinchupe@icloud.com>
2018-12-08 20:39:06 -0600

    Add libs for Movie Service.

---
+++ app/src/main/java/com/treken/cyberflix/BaseApplication.java
@@ -0,0 +1,16 @@
+package com.treken.cyberflix;
+
+import android.app.Application;
+import android.os.StrictMode;
+/**
+ * @author arun
+ */
+public class BaseApplication extends Application
+{
+    @Override
+    public void onCreate()
+    {
+        super.onCreate();
+        StrictMode.enableDefaults();
+    }
+}
---
+++ app/src/main/java/com/treken/cyberflix/MovieService.java
@@ -0,0 +1,8 @@
+package com.treken.cyberflix;
+
+/**
+ * Created by arun on 12/6/15.
+ */
+public class MovieService
+{
+}
